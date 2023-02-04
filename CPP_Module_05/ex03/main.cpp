1a278bc1673b99e9ca0ec55d94254d4dc0df4832
Mohammed Ehsan Ullah Khan <mehsankhan02@gmail.com>
2023-02-04 15:25:35 +0400

    CPP_MODULE_05 ex02

--- CPP_Module_05/ex03/main.cpp
+++ CPP_Module_05/ex03/main.cpp
@@ -6,54 +6,87 @@
 /*   By: mkhan <mkhan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 16:57:00 by mkhan             #+#    #+#             */
-/*   Updated: 2023/01/29 19:37:27 by mkhan            ###   ########.fr       */
+/*   Updated: 2023/01/30 17:07:24 by mkhan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Bureaucrat.hpp"
+#include "Intern.hpp"
 
 int main()
 {
 	{
+		std::string formName = "presidential pardon"; 
+		std::string formTarget = "Tae";
+		if (formName.empty() || formTarget.empty()) 
+		{
+				throw std::invalid_argument("Invalid parameters");
+		}
 		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-		Bureaucrat A("Ram", 151);
+		Intern intern;
 		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-	}
-	{
+		Bureaucrat bureaucrat("Adam", 1);
 		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-		Bureaucrat C("One", 5);
+		AForm *f = intern.makeForm(formName, formTarget);
 		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-		C.gradeDecreament();
+		std::cout << *f;
 		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-		std::cout << C;
+		bureaucrat.signAForm(*f);
 		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-	}
-	{
+		std::cout << (*f).getSigned() << std::endl;
 		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-		Bureaucrat C("Tom",1);
+		bureaucrat.executeForm(*f);
 		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-		C.gradeIncreament();
-		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-		std::cout << C;
-		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-	}
-	{
-		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-		Bureaucrat B("john", 151);
-		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-		B.gradeIncreament();
-		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-		std::cout << B;
+		delete f;
 		std::cout << "-----------------------------------------------------------------------------" << std::endl;
 	}
 	{
+		std::string formName = "robotomy request"; 
+		std::string formTarget = "Bender";
+		if (formName.empty() || formTarget.empty()) 
+		{
+				throw std::invalid_argument("Invalid parameters");
+		}
 		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-		Bureaucrat D("Adam", 150);
+		Intern intern;
 		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-		D.gradeDecreament();
+		Bureaucrat bureaucrat("Hope", 1);
 		std::cout << "-----------------------------------------------------------------------------" << std::endl;
-		std::cout << D;
+		AForm *f = intern.makeForm(formName, formTarget);
+		std::cout << "-----------------------------------------------------------------------------" << std::endl;
+		std::cout << *f;
+		std::cout << "-----------------------------------------------------------------------------" << std::endl;
+		bureaucrat.signAForm(*f);
+		std::cout << "-----------------------------------------------------------------------------" << std::endl;
+		std::cout << (*f).getSigned() << std::endl;
+		std::cout << "-----------------------------------------------------------------------------" << std::endl;
+		bureaucrat.executeForm(*f);
+		std::cout << "-----------------------------------------------------------------------------" << std::endl;
+		delete f;
 		std::cout << "-----------------------------------------------------------------------------" << std::endl;
 	}
-	return (0);
+	{
+		std::string formName = "shrubbery creation"; 
+		std::string formTarget = "Gorms";
+		if (formName.empty() || formTarget.empty()) 
+		{
+				throw std::invalid_argument("Invalid parameters");
+		}
+		std::cout << "-----------------------------------------------------------------------------" << std::endl;
+		Intern intern;
+		std::cout << "-----------------------------------------------------------------------------" << std::endl;
+		Bureaucrat bureaucrat("John", 1);
+		std::cout << "-----------------------------------------------------------------------------" << std::endl;
+		AForm *f = intern.makeForm(formName, formTarget);
+		std::cout << "-----------------------------------------------------------------------------" << std::endl;
+		std::cout << *f;
+		std::cout << "-----------------------------------------------------------------------------" << std::endl;
+		bureaucrat.signAForm(*f);
+		std::cout << "-----------------------------------------------------------------------------" << std::endl;
+		std::cout << (*f).getSigned() << std::endl;
+		std::cout << "-----------------------------------------------------------------------------" << std::endl;
+		bureaucrat.executeForm(*f);
+		std::cout << "-----------------------------------------------------------------------------" << std::endl;
+		delete f;
+		std::cout << "-----------------------------------------------------------------------------" << std::endl;
+	}
 }
