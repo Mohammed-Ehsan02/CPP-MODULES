c5e8c56f9f3ec8636d3fc66e583ed30861107c5b
Mohammed ehsan ullah Khan <mkhan@lab3r4s11.42abudhabi.ae>
2023-02-16 18:09:57 +0400

    CPP06 ex01 fix

--- CPP_Module_06/ex01/Serialize.hpp
+++ CPP_Module_06/ex01/Serialize.hpp
@@ -6,7 +6,7 @@
 /*   By: mkhan <mkhan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 13:54:11 by mkhan             #+#    #+#             */
-/*   Updated: 2023/02/16 15:59:08 by mkhan            ###   ########.fr       */
+/*   Updated: 2023/02/16 18:09:01 by mkhan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <iostream>
 # include <string>
 # include <cstdlib>
-# include <cstdint>
+# include <stdint.h>
 struct Data
 {
 	std::string	s;
