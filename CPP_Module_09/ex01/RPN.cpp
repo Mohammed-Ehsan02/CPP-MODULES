b621933fd39904e2c15e03ae8ef46502186ceb25
Mohammed Ehsan Ullah Khan <mehsankhan02@gmail.com>
2023-07-09 00:00:27 +0400

    ex01 done

---
+++ CPP_Module_09/ex01/RPN.cpp
@@ -0,0 +1,74 @@
+#include "RPN.hpp"
+
+RPN::RPN()
+{
+}
+
+RPN::RPN(std::string argv)
+{
+	if (argv.size() > 3)
+	{
+        this->fillStack(argv);
+	    std::cout << this->_rpn_num.top() << std::endl;
+    }
+}
+
+RPN::RPN(const RPN &object)
+{
+    *this = object;
+}
+
+RPN &RPN::operator=(const RPN &rhs)
+{
+    if (this != &rhs)
+    {
+		this->_rpn_stack = rhs._rpn_stack;
+		this->_rpn_num = rhs._rpn_num;
+    }
+    return (*this);
+}
+
+RPN::~RPN()
+{
+}
+
+
+void	RPN::calculate()
+{
+	int	operand1, operand2;
+	char operation;
+
+	operand2 = this->_rpn_num.top();
+	this->_rpn_num.pop();
+	operand1 = this->_rpn_num.top();
+	this->_rpn_num.pop();
+	operation = this->_rpn_stack.top();
+	this->_rpn_stack.pop();
+	if (operation == '*')
+		this->_rpn_num.push(operand1 * operand2);
+	else if (operation == '/')
+		this->_rpn_num.push(operand1 / operand2);
+	else if (operation == '-')
+		this->_rpn_num.push(operand1 - operand2);
+	else if (operation == '+')
+		this->_rpn_num.push(operand1 + operand2);
+}
+
+void	RPN::fillStack(std::string expr)
+{
+	int	expr_size;
+
+	expr_size = expr.size();
+	for (int i = 0; i < expr_size; i++)
+	{
+		if (expr[i] != ' ')
+		{
+			if (isdigit(expr[i]) != 0)
+				this->_rpn_num.push(expr[i] - '0');
+			else
+				this->_rpn_stack.push(expr[i]);
+			if (this->_rpn_stack.size() == 1 && this->_rpn_num.size() >= 2)
+				calculate();
+		}
+	}
+}
